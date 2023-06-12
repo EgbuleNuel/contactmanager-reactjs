ff8c49b4ae26f9c58aea249a377d8d7ff5c69144
EgbuleNuel <egbulemmanuel@gmail.com>
2023-06-12 01:14:24 +0100

    imported bootstrap into project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,21 @@ function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <Contact name="John Doe" email="jdoe@gmail.com" phone="0234 234 2342" />
+      <Contact
+        name="John Doe"
+        email="jdoe@gmail.com"
+        phone="0234 234 2342"
+      />
+      <Contact
+        name="Karen Smith"
+        email="ksmith@gmail.com"
+        phone="5555 555 5555"
+      />
+      <Contact
+        name="Jimmy Hoffa"
+        email="jhoffa@gmail.com"
+        phone="1234 567 8901"
+      />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
 const Header = () => {
+  const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 };
