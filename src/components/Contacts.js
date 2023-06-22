1911df642a5c769706744c317c5dd5367c93590c
EgbuleNuel <egbulemmanuel@gmail.com>
2023-06-22 19:38:40 +0100

    Using The Context API & Provider State

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,30 +1,9 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
   // the constructor to
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "0234 234 2342",
-      },
-      {
-        id: 2,
-        name: "Karen Smith",
-        email: "ksmith@gmail.com",
-        phone: "5555 555 5555",
-      },
-      {
-        id: 3,
-        name: "Jimmy Hoffa",
-        email: "jhoffa@gmail.com",
-        phone: "1234 567 8901",
-      },
-    ],
-  };
 
   deleteContact = (id) => {
     const { contacts } = this.state;
@@ -34,17 +13,23 @@ class Contacts extends Component {
   };
 
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map((contact) => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {(value) => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map((contact) => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
---
+++ src/context.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "0234 234 2342",
+      },
+      {
+        id: 2,
+        name: "Karen Smith",
+        email: "ksmith@gmail.com",
+        phone: "5555 555 5555",
+      },
+      {
+        id: 3,
+        name: "Jimmy Hoffa",
+        email: "jhoffa@gmail.com",
+        phone: "1234 567 8901",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
